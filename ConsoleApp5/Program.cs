0dd35b8b977afbc077b3e1fd50bd1d78f537e30f
Alex195 <Sanis1995@mail.ru>
2018-05-29 10:28:53 +0700

    add program.cs

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ConsoleApp5
+{
+
+        
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    var assembly = Assembly.GetExecutingAssembly();
+                    var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+                    var version = fileVersionInfo.ProductVersion;
+                    Console.Write(fileVersionInfo);
+                    Console.ReadLine();
+    
+                }
+
+            }
+}
